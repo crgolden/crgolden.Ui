f50fac7447f97d77af4ec7c01d03ef56135edd6d
Chris Golden <crgolden@msn.com>
2018-12-06 05:56:05 -0600

    Updating prod clientId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://clarity-api.azurewebsites.net',
   identityUrl: 'https://clarity-oidc.azurewebsites.net',
-  identityClientId: '9aa08baf-4def-41e4-9384-324766d9227f'
+  identityClientId: '65b11ca7-7ba7-497c-902e-c23f18f47e20'
 };
